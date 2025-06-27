26694f5dae8c1d4df8c2bc9d8ac3d918042dfba7
Apparat4ik <apparat.inc45@gmail.com>
2025-06-27 21:27:57 +0700

    Initial Commit

---
+++ Lab_3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab_3
+//
+//  Created by Владислав on 27.06.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
